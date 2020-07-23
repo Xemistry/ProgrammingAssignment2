aa22920470ae47ea0bbc06470fd8672ec3a41760
Heinrich Soliven <68130276+Xemistry@users.noreply.github.com>
2020-07-24 01:55:15 +0800

    My programming assignment 2!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The "makeCacheMatrix" function shows a list of functions used by "cacheSolve"
+## to set and get the matrix to be inverted. "set" creates the matrix.
+## "get" stores the value of the matrix into the cache. "setinverse" inverts the
+## matrix from the cache. "getinverse"gets inverted matrix from the cache. The
+## created functions are then shown as a list.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  storage <- NULL
+    set <- function(y) {
+    x <<- y
+    storage <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) storage <<- inverse
+  getinverse <- function() storage
+    list(set=set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The "cacheSolve" calculates the inverse of the matrix created from the
+## "makeCacheMatrix" function. The if statement makes sure to return
+## the inverted matrix if it exists. The matrix is then created and stored
+## in the "matrix" variable.
+## Assumption: Matrx inputted is always invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if (!is.null(cache)) {
+    message("getting cached data")
+    return(storage)
+  }
+    matrix <- x$get()
 }
